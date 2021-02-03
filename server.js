6c2f179754992642ca7a9764304b79bf9595ff06
DevHerrera <cesarchavarria0@gmail.com>
2021-02-02 23:05:01 -0600

    Add express to render react app

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, '.', 'public');
+
+// app.use(express.static(__dirname, ''));
+app.use(express.static(path.join(__dirname, './build')))
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/asd', function (req, res) {
+	res.sendFile(path.join(__dirname, './build'), 'index.html')
+
+});
+
+app.listen(process.env.PORT || 8080);
