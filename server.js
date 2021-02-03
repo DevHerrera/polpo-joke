a51db19a1107b7e45be83a4d5648f91c33e4437f
DevHerrera <cesarchavarria0@gmail.com>
2021-02-02 23:26:47 -0600

    minimal fix to deploy on heroku

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.get('/asd', function (req, res) {
 
 });
 
-app.listen(3000, () => {
-	console.log(`App running on port 3000`) 
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`App running`) 
 });
